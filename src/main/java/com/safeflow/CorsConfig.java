a9d0547efa7096d36655c1802a4fad6592d2e25f
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-07-05 09:57:38 -0500

    feat:added cors

--- src/main/java/com/safeflow/CorsConfig.java
+++ src/main/java/com/safeflow/CorsConfig.java
@@ -1,30 +1,19 @@
 package com.safeflow;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Configuration class for Cross-Origin Resource Sharing (CORS) in the GreenGrow backend application.
- * @author GrowGenius
- * @version 1.0 19/11/2023
- */
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-    /**
-     * Configures Cross-Origin Resource Sharing (CORS) for the application.
-     *
-     * @param registry The CorsRegistry to configure CORS for the application.
-     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://safeflow-v1.netlify.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
+                .allowedOrigins("*")           // Permite cualquier origen
+                .allowedMethods("*")           // Permite cualquier método (GET, POST, PUT, DELETE, OPTIONS, etc)
+                .allowedHeaders("*")           // Permite cualquier header
+                .allowCredentials(false)       // No permite credenciales cuando origins="*"
                 .maxAge(3600);
     }
 }
